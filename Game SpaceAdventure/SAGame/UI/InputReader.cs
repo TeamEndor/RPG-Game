540ee05e55a463bc9d14186c2f1a21977828b4cc
223orange <iliya.naydenov@gmail.com>
2015-12-19 12:13:49 +0200

    merging

---
+++ Game SpaceAdventure/SAGame/UI/InputReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAGame.Interfaces;
+
+namespace SAGame.UI
+{
+    public class InputReader : IInputReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
